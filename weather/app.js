d00ccedef0c6c33fdbd28320eacd3cc15693a476
Biancano <bebostunner234@gmail.com>
2018-07-18 18:50:01 -0700

    Create app.js

---
+++ weather/app.js
@@ -0,0 +1,28 @@
+function weather() {
+  var location = document.getElementById("location");
+  var apiKey = "23bc5a73297ae0b4f0f2aa05de4f1ba0";
+  var url = "https://api.forecast.io/forecast/";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    $.getJSON(
+      url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
+      function(data) {
+        $("#temp").html(data.currently.temperature + "° F");
+        $("#minutely").html(data.minutely.summary);
+      }
+    );
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+  location.innerHTML = "Locating...";
+}
+
+weather();
