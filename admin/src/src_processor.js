41220e594a2b555c19a494a0cfe26b78bebdde6f
Biancano <bebostunner234@gmail.com>
2018-11-08 18:37:55 -0800

    Update src_processor.js

--- admin/src/src_processor.js
+++ admin/src/src_processor.js
@@ -3,7 +3,22 @@ fetch(request).then(function(response) {
   console.log(response.status); // returns 200
   response.blob().then(function(myBlob) {
     var objectURL = URL.createObjectURL(myBlob);
-    myImage.src = objectURL;
+    object.src = objectURL;
   });
+  return response.status;
 });
+}
+function name(obj, req){
+  var etype, enum;
+  var x = test(obj, req);
+  if(x[0] == 1){
+    etype = "A";
+    var a = 1;
+    var bool = false;
+    while(bool == false){
+      if(x[2] == 1a){
+        enum = "1";  
+      }
+    }
+  }
 }
