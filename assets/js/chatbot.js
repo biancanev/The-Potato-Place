085d7f1af39d1afc5aec7cd86358656f70e7872e
Biancano <bebostunner234@gmail.com>
2020-01-30 18:13:26 -0800

    Update chatbot.js

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -1,1 +1,9 @@
-
+function check_keywords(string){
+  if(string.toLowerCase().includes("") == true){
+    gantt(string);
+  }
+}
+function gantt(string){
+  var index = string.indexOf("gantt") + 6;
+  var res = string.substring(index, )
+}
